463eadb1921a37f29bfb86d3667ade84cda0ce31
Shou Hao, Zhao <j5201314peter@gmail.com>
2017-10-31 14:25:03 +0800

    memory cache

--- api.js
+++ api.js
@@ -1,22 +1,35 @@
 import mongoose from 'mongoose'
+import cache from 'memory-cache'
 import Car from './model/car'
 import Record from './model/record'
 
 export const getCarList = (req, res) => {
-  Car.find({}, (err, carList) => {
+  const carlist = cache.get('carlist')
+  if (carlist)
+    return res.json(carlist)
+  else Car.find({}, (err, carList) => {
     if(err) {
       console.error(err);
       return res.status(400).send(err)
     }
+    cache.put('carlist', carList)
     return res.json(carList)
   })
 }
+export const postCar = (req, res) => {
+  const { ...car } = req.body;
+  const NewCar = new Car(car);
+  NewCar.save(err => err ? console.error(err)&& res.send(err) : res.send('OK'))
+}
 export const getAbnormal = (req, res) => {
-  Record.find({}, (err, rList) => {
+  const rList = cache.get('rList')
+  if (rList) return res.json(rList)
+  else Record.find({}, (err, rList) => {
     if(err) {
       console.error(err);
       return res.status(400).send(err)
     }
+    cache.put('rList', rList)
     return res.json(rList)
   })
 }
